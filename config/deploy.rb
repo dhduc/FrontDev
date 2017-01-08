71481a9e7126d1459b9cf522c0bdc3dd5dff590d
Duc Dao <huuduc.uneti@gmail.com>
2017-01-08 19:30:29 +0700

    Update Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,24 +1,24 @@
 # config valid only for current version of Capistrano
 lock "3.7.1"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "frontdev"
+set :repo_url, "git@github.com:dhduc/FrontDev.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/var/www/frontdev"
 
 # Default value for :format is :airbrussh.
-# set :format, :airbrussh
+set :format, :pretty
 
 # You can configure the Airbrussh format using :format_options.
 # These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
 
 # Default value for :pty is false
-# set :pty, true
+set :pty, true
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml", "config/secrets.yml"
