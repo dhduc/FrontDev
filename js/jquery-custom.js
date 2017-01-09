8434f8799d19cd77701cd96e0dd6fbe9bbeef06d
huongtt <huongtt@smartosc.com>
2017-01-09 14:29:31 +0700

    update ex-02-slider

--- js/jquery-custom.js
+++ js/jquery-custom.js
@@ -1,5 +1,5 @@
 // Mouseover --> hand cusor
-// Click --> hide 
+// Click --> hide   - Ex-01
 var obj=".intro-message h1";
 $(obj).mouseover(function(){
 	$(this).css("background","red");
@@ -17,8 +17,24 @@ $(obj).click(function(){
 
 
 
-// accordion
-var acc = document.getElementsByClassName("accordion");
-var cont= document.getElementsByClassName("container");
-var i;
+// accordion - Ex 02
 
+var jQueryAcc = $(".accordion");
+
+$(".accordion").click(function(){
+	$(this).next().toggleClass('active');
+});
+
+
+
+// Slider - ex03
+$("#slideshow > li.item:gt(0)").hide();
+
+setInterval(function() {
+  $('#slideshow > li.item:first') // first item 
+    .fadeOut(1000) // first item out
+    .next() // call second item
+    .fadeIn(1000) // show second item
+    .end() // Finish second item   
+    .appendTo('#slideshow');// continue slide 
+}, 1000);
